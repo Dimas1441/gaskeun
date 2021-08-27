f4583e3caabd32ed98d2a6710fda9599cc64b688
Dimas1441 <89393904+Dimas1441@users.noreply.github.com>
2021-08-28 02:25:24 +0700

    Add files via upload

--- Panel-wireguard/add-wg.sh
+++ Panel-wireguard/add-wg.sh
@@ -25,48 +25,48 @@ SERVER_PUB_IP=$(cat /etc/v2ray/domain);
 else
 SERVER_PUB_IP=$IP
 fi
-	
-	until [[ ${CLIENT_NAME} =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "Username: " -e CLIENT_NAME
-		CLIENT_EXISTS=$(grep -w $CLIENT_NAME /etc/wireguard/wg0.conf | wc -l)
 
-		if [[ ${CLIENT_EXISTS} == '1' ]]; then
-			echo ""
-			echo "Nama Sudah Ada, Harap Masukkan Nama Lain!"
-			exit 1
-		fi
-	done
-	ENDPOINT="$SERVER_PUB_IP:$SERVER_PORT"
-	WG_CONFIG="/etc/wireguard/wg0.conf"
-	LASTIP=$( grep "/32" $WG_CONFIG | tail -n1 | awk '{print $3}' | cut -d "/" -f 1 | cut -d "." -f 4 )
-	if [[ "$LASTIP" = "" ]]; then
-	CLIENT_ADDRESS="10.66.66.2"
-	else
-	CLIENT_ADDRESS="10.66.66.$((LASTIP+1))"
-	fi
+until [[ ${CLIENT_NAME} =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+read -rp "Username: " -e CLIENT_NAME
+CLIENT_EXISTS=$(grep -w $CLIENT_NAME /etc/wireguard/wg0.conf | wc -l)
 
-	# Adguard DNS by default
-	CLIENT_DNS_1="176.103.130.130"
+if [[ ${CLIENT_EXISTS} == '1' ]]; then
+echo ""
+echo "Nama Sudah Ada, Harap Masukkan Nama Lain!"
+exit 1
+fi
+done
+ENDPOINT="$SERVER_PUB_IP:$SERVER_PORT"
+WG_CONFIG="/etc/wireguard/wg0.conf"
+LASTIP=$( grep "/32" $WG_CONFIG | tail -n1 | awk '{print $3}' | cut -d "/" -f 1 | cut -d "." -f 4 )
+if [[ "$LASTIP" = "" ]]; then
+CLIENT_ADDRESS="10.66.66.2"
+else
+CLIENT_ADDRESS="10.66.66.$((LASTIP+1))"
+fi
 
-	CLIENT_DNS_2="176.103.130.131"
-	
-	read -p "Expired (hari): " masaaktif
-	exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-        tgl=$(date -d "$masaaktif days" +"%d")
-        bln=$(date -d "$masaaktif days" +"%b")
-        thn=$(date -d "$masaaktif days" +"%Y")
-        expe="$tgl $bln, $thn"
-        tgl2=$(date +"%d")
-        bln2=$(date +"%b")
-        thn2=$(date +"%Y")
-        tnggl="$tgl2 $bln2, $thn2"
-	# Generate key pair for the client
-	CLIENT_PRIV_KEY=$(wg genkey)
-	CLIENT_PUB_KEY=$(echo "$CLIENT_PRIV_KEY" | wg pubkey)
-	CLIENT_PRE_SHARED_KEY=$(wg genpsk)
+# Adguard DNS by default
+CLIENT_DNS_1="176.103.130.130"
 
-	# Create client file and add the server as a peer
-	echo "[Interface]
+CLIENT_DNS_2="176.103.130.131"
+
+read -p "Expired (hari): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
+tgl2=$(date +"%d")
+bln2=$(date +"%b")
+thn2=$(date +"%Y")
+tnggl="$tgl2 $bln2, $thn2"
+# Generate key pair for the client
+CLIENT_PRIV_KEY=$(wg genkey)
+CLIENT_PUB_KEY=$(echo "$CLIENT_PRIV_KEY" | wg pubkey)
+CLIENT_PRE_SHARED_KEY=$(wg genpsk)
+
+# Create client file and add the server as a peer
+echo "[Interface]
 PrivateKey = $CLIENT_PRIV_KEY
 Address = $CLIENT_ADDRESS/24
 DNS = $CLIENT_DNS_1,$CLIENT_DNS_2
@@ -77,34 +77,31 @@ PresharedKey = $CLIENT_PRE_SHARED_KEY
 Endpoint = $ENDPOINT
 AllowedIPs = 0.0.0.0/0,::/0" >>"$HOME/$SERVER_WG_NIC-client-$CLIENT_NAME.conf"
 
-	# Add the client as a peer to the server
-	echo -e "### Client $CLIENT_NAME $exp
+# Add the client as a peer to the server
+echo -e "### Client $CLIENT_NAME $exp
 [Peer]
 PublicKey = $CLIENT_PUB_KEY
 PresharedKey = $CLIENT_PRE_SHARED_KEY
 AllowedIPs = $CLIENT_ADDRESS/32" >>"/etc/wireguard/$SERVER_WG_NIC.conf"
-	systemctl restart "wg-quick@$SERVER_WG_NIC"
-	cp $HOME/$SERVER_WG_NIC-client-$CLIENT_NAME.conf /home/vps/public_html/$CLIENT_NAME.conf
-	clear
-        sleep 1
-	echo -e ""
-	echo -e "${red}=================================${off}"
-        echo -e "${white} WIREGUARD${off}"
-        echo -e "${red}=================================${off}"
-        echo -e " ${white}ISP            : ${ISP}"
-        echo -e " CITY           : ${CITY}"
-        echo -e " COUNTRY        : ${COUNTRY}"
-        echo -e " Server IP      : ${MYIP}"
-        echo -e " Server Host    : ${SERVER_PUB_IP}${off}"
-        echo -e "${red}=================================${off}"
-	echo -e " ${white}WireGuard URL  : "
-	echo -e ""
-    echo -e " http://$SERVER_PUB_IP:7070/$CLIENT_NAME.conf${off}"
-	echo -e ""
-    echo -e "${red}=================================${off}"
-	echo -e " ${white}Aktif Selama   : $masaaktif Hari"
+systemctl restart "wg-quick@$SERVER_WG_NIC"
+cp $HOME/$SERVER_WG_NIC-client-$CLIENT_NAME.conf /home/vps/public_html/$CLIENT_NAME.conf
+clear
+sleep 1
+echo -e ""
+echo -e "${red}=================================${off}"
+echo -e "${white} WIREGUARD${off}"
+echo -e "${red}=================================${off}"
+echo -e " ${white}ISP            : ${ISP}"
+echo -e " CITY           : ${CITY}"
+echo -e " COUNTRY        : ${COUNTRY}"
+echo -e " Server IP      : ${MYIP}"
+echo -e " Server Host    : ${SERVER_PUB_IP}${off}"
+echo -e "${red}=================================${off}"
+echo -e " ${white}WireGuard URL  : "
+echo -e " http://$SERVER_PUB_IP:81/$CLIENT_NAME.conf${off}"
+echo -e "${red}=================================${off}"
+echo -e " ${white}Aktif Selama   : $masaaktif Hari"
 echo -e " Dibuat Pada    : $tnggl"
 echo -e " Berakhir Pada  : $expe${off}"
 echo -e "${red}=================================${off}"
-echo -e ""
-	rm -f /root/wg0-client-$CLIENT_NAME.conf
+rm -f /root/wg0-client-$CLIENT_NAME.conf
